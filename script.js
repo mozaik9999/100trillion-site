46922c8c4b8a99a09a0fdc45d3870e8b1ac05bfc
mozaik9999 <fukuiosaka305@icloud.com>
2025-03-19 10:30:21 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -31,10 +31,7 @@ function addItem() {
     document.getElementById("itemPrice").value = "";
     document.getElementById("itemQuantity").value = "";
     
-    const number = 1234567;
-const formatted = number.toLocaleString();  // "1,234,567" が得られる
-console.log(formatted);
-
+    
 
 
 
